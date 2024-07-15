21027db36aaa51b074d222898c179ed982ea92ab
Doğan ŞAHİN <dogansahiin@gmail.com>
2024-07-15 15:18:17 +0300

    Odev Sorulari Cozuldu

--- Odev-8.js
+++ Odev-8.js
@@ -7,3 +7,18 @@
 
 Örnek Dizi: [-1, -2, -3, -4, -5]
 */
+
+//S-1
+
+function filterByLength(words) {
+    return words.filter(word => word.length === 3);
+  }
+  let words = ['cat', 'dog', 'elephant', 'rat', 'bat'];
+  let filteredWords = filterByLength(words);
+  console.log(filteredWords);
+
+  //S-2
+  let numbers = [-1, -2, -3, -4, -5];
+  let absoluteValues = numbers.map(number => Math.abs(number));
+  console.log(absoluteValues);
+
